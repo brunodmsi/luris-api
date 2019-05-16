88430de87fc6728e0afb58568ab4658ecda630c5
Bruno De Masi <bruno.demasi@hotmail.com>
2019-05-15 22:01:36 -0300

    add nome usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,6 +4,10 @@ const jwt = require('jsonwebtoken')
 const authConfig = require('../../config/auth')
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
